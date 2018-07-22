ae7c1f8065d2754e4fbc3b42cef38970abbdf5d4
againafter10 <archana.joshi@gmail.com>
2018-07-23 09:27:57 +1000

    Update cloudstorage.py

--- cloudstorage.py
+++ cloudstorage.py
@@ -1,6 +1,5 @@
-cloudstorage.py
-
-
+# compare files in 2 different blobs and check if their name and count match
+# get the leaf files in each of the sub folders
 
 import os, uuid, sys
 from azure.storage.blob import BlockBlobService, PublicAccess
@@ -11,11 +10,6 @@ import azure.storage.blob as azureblob
 import config
 
 
-# STORAGE_ACCOUNT_NAME = "dataplatdevblob00"
-# STORAGE_ACCOUNT_KEY = "sqI+1LLilEvjdGSgumF85X8zlgTLYHmg0JUbPew2akdl/Vc93B1ceRouIGQqbwndkFnGETgnRiJ3GRWcRDlBiw=="
-
-# storageName = "dataplatdevblob03"
-# storageKey = "/7fNjm9gJYMNWS4DexNvDrnHgMUJOJWY6EoJYMs3YuWZcWp+jFZyDXHTpaymm9Gt+C6Z6uGitrRV6MCS9oCz7A=="
 
 def file_list_blob00(container_name,blob_path):
     storageName = 'dataplatdevblob00'
@@ -34,26 +28,24 @@ def file_list_blob03(container_name,blob_path):
     bbs = storage.create_block_blob_service()
     return(bbs.list_blobs(container_name,blob_path)) 
 
-
+# compare files in 2 different blobs and check if their name and count match
 def compare_blob_structure(file_list_blob00,file_list_blob03):
     if file_list_blob00 == file_list_blob03 :
         """
         for i in file_list:
              print(i.name)
-        #get_leaf_files(file_list_blob00)
         """
     else:
         print("Blob structure mismatch.....")
 
-
+# get the leaf files in each of the sub folders
 def get_leaf_files(file_list):
     name_split = []
     name_d = {}
-    # split on the last occurence of the delimiter "/" to get the file names and the parent folder to find latest schema version 
+    # split on the last occurence of the delimiter "/" to get the file names and the parent folder
+    # to find latest schema version 
     for i in file_list:
         name_split.append(i.name.rsplit('/',1))
-    
-        
     
     for i in range(0,len(name_split)):
         if name_split[i][0] in name_d:
@@ -76,15 +68,14 @@ def get_leaf_files(file_list):
 #####           MAIN            #####
 #####################################
 
-blob_path = 'nbv/in_nbv_azure_net_transaction/'
-blob03_path = 'nbv/in_nbv_azure_net_transaction/'
-blob00_path = 'nbv/net_transaction/'
-"""
+blob_path = 'source1/data_sub_type/'
+blob03_path = 'source1/data_sub_type/'
+blob00_path = 'source1/data_sub_type/'
+
 file_list_blob00 = file_list_blob00('raw' , blob_path)
 file_list_blob03 = file_list_blob03('raw' , blob_path)
-
 compare_blob_structure(file_list_blob00,file_list_blob03)
-"""
+
 file_list_blob00 = file_list_blob00('raw' , blob00_path)
 file_list_blob03 = file_list_blob03('raw' , blob03_path)
 
