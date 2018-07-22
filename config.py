e339d6d55fa2c2e313f1b1a11a88bf2901d1d06e
againafter10 <archana.joshi@gmail.com>
2018-07-23 09:17:29 +1000

    Add files via upload

---
+++ config.py
@@ -0,0 +1,18 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1561\cocoasubrtf400
+{\fonttbl\f0\fmodern\fcharset0 CourierNewPSMT;}
+{\colortbl;\red255\green255\blue255;\red15\green112\blue1;\red255\green255\blue255;\red0\green0\blue0;
+\red144\green1\blue18;}
+{\*\expandedcolortbl;;\cssrgb\c0\c50196\c0;\cssrgb\c100000\c100000\c100000;\cssrgb\c0\c0\c0;
+\cssrgb\c63922\c8235\c8235;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11900\viewh12720\viewkind1
+\deftab720
+\pard\pardeftab720\sl320\partightenfactor0
+
+\f0\fs28 \cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 # define the keys for variuos strorage accounts here\cf4 \strokec4 \
+\
+storage =\{\
+    \cf5 \strokec5 "datablob00"\cf4 \strokec4 : \cf5 \strokec5 "XXXXXXXXXXXXXXXXX=="\cf4 \strokec4  ,\
+    \cf5 \strokec5 "datablob01\'94\cf4 \strokec4  : \cf5 \strokec5 "XXXXXXXXXXXXXXXXXX=="\cf4 \strokec4 \
+\}\
+}
