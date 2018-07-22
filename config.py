4c0002d3d538fa273fb04f50db0daf2595ea9883
againafter10 <archana.joshi@gmail.com>
2018-07-23 09:21:16 +1000

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # define the keys for variuos strorage accounts here
 
 storage ={
-    "dataplatdevblob00": "XXXXXXXXXXXXXXXXXTLYHmg0JUbPew2akdl/Vc93B1ceRouIGQqbwndkFnGETgnRiJ3GRWcRDlBiw==" ,
-    "dataplatdevblob03" : "XXXXXXXXXXXXXXXXXXHgMUJOJWY6EoJYMs3YuWZcWp+jFZyDXHTpaymm9Gt+C6Z6uGitrRV6MCS9oCz7A=="
+    "datablob00": "XXXXXXXXXXXXXXXXX==" ,
+    "datablob01" : "XXXXXXXXXXXXXXXXXX=="
 }
